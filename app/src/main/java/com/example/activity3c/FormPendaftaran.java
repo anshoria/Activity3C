73791e7c033e340566241b78aea0c0e216fbcde6
anshoria <anshoriakbar11@gmail.com>
2021-04-01 17:27:05 +0700

    Pesan komit wajib diisi (Commit Message Required) Ex: Commit 1 (publish projek ke dalam github (publish the project to github))

---
+++ app/src/main/java/com/example/activity3c/FormPendaftaran.java
@@ -0,0 +1,57 @@
+package com.example.activity3c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class FormPendaftaran extends AppCompatActivity {
+    EditText edtNama, edtAlamat, edtEmail, edtPassword, edtrepass;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_pendaftaran);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabSimpan);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtNama.getText().toString().isEmpty() ||
+                        edtAlamat.getText().toString().isEmpty() ||
+                        edtEmail.getText().toString().isEmpty() ||
+                        edtPassword.getText().toString().isEmpty() ||
+                        edtrepass.getText().toString().isEmpty())
+                {
+
+                    Snackbar.make(v,"Wajib isi seluruh data !!!", Snackbar.LENGTH_LONG).show();
+                }
+                else {
+
+                    if (edtPassword.getText().toString().equals(edtrepass.getText().toString()))
+                    {
+
+                        Toast.makeText(getApplicationContext(), "Pendaftaran Berhasil...",
+                                Toast.LENGTH_LONG).show();
+
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                    else
+                    {
+
+                        Snackbar.make(v, "Password dan Repassword harus sama!!!!",
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+}
